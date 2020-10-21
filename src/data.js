deac532c2f173e56897d50b38b03f335a670ca48
PryakhinMS <pryakhin.ms@gmail.com>
2020-10-21 19:43:20 +0400

    adding data

---
+++ src/data.js
@@ -0,0 +1,7 @@
+const data = [
+    {id: 1, brand: 'HUAWEI', model: 'P smart Z', price: 162, description: ''},
+    {id: 2, brand: 'HONOR', model: '9X', price: 171, description: ''},
+    {id: 3, brand: 'XIAOMI', model: 'Redmi Note 8 Pro ', price: 198, description: ''},
+    {id: 4, brand: 'MEIZU', model: 'X8', price: 170, description: ''},
+    {id: 5, brand: 'SAMSUNG', model: 'GALAXY A01', price: 10000, description: ''}
+];
